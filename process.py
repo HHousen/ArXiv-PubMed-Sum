3f3189ff244e4fc1d00e875069a39ee35acd0e4d
Hayden Housen <hayden@haydenhousen.com>
2020-04-28 11:51:19 -0400

    Remove extra newlines and add an assertion that the ouput is the same length as the input

--- process.py
+++ process.py
@@ -6,18 +6,21 @@ import json
 from time import time
 from tqdm import tqdm
 
+
 def write_to_bin(article_dir, save_path):
     dataset = {
         "train": {"source": [], "target": []},
         "val": {"source": [], "target": []},
-        "test": {"source": [], "target": []}
+        "test": {"source": [], "target": []},
     }
     splits = glob.glob(os.path.join(article_dir, "*.txt"))
     for split in tqdm(splits, desc="Loading Splits"):
         with open(split, "r") as articles_info:
             # count the number of lines in the file
             try:
-                print("Counting the number of lines for accurate progress bar. Press CTRL+C to cancel line counting.")
+                print(
+                    "Counting the number of lines for data integrity and accurate progress bar. Press CTRL+C to cancel line counting (not recommended)."
+                )
                 t0 = time()
                 num_articles = sum(1 for line in articles_info)
                 # reset pointer to the beginning of the file
@@ -27,7 +30,9 @@ def write_to_bin(article_dir, save_path):
                 print("Skipping line counting...")
             articles_info.seek(0)
 
-            for article_info in tqdm(articles_info, desc="Loading Articles", total=num_articles):
+            for article_info in tqdm(
+                articles_info, desc="Loading Articles", total=num_articles
+            ):
                 article_info = json.loads(article_info)
                 abstract_sents = article_info["abstract_text"]
                 article_sents = article_info["article_text"]
@@ -35,21 +40,48 @@ def write_to_bin(article_dir, save_path):
                 # remove the <S> and </S> tokens
                 abstract_sents = [x[4:-4] for x in abstract_sents]
 
+                # remove newlines
+                # each document is sentence-tokenized
+                abstract = [sentence for sentence in abstract_sents if sentence != "\n"]
+                article = [sentence for sentence in article_sents if sentence != "\n"]
+
                 # convert from lists of sentences to strings
-                abstract = " ".join(abstract_sents).strip()
-                article = " ".join(article_sents).strip()
+                abstract = " ".join(abstract).strip()
+                article = " ".join(article).strip()
 
                 split_name = os.path.splitext(os.path.basename(split))[0]
 
                 dataset[split_name]["source"].append(article)
                 dataset[split_name]["target"].append(abstract)
 
+            # if the number of articles were counted then make sure it equals the number of processed articles
+            if num_articles:
+                assert (
+                    num_articles
+                    == len(dataset[split_name]["source"])
+                    == len(dataset[split_name]["target"])
+                ), (
+                    "The number of processed articles does not equal the number of input articles. num_articles is "
+                    + str(num_articles)
+                    + ", number of sources is "
+                    + str(len(dataset[split_name]["source"]))
+                    + ", and number of targets is "
+                    + str(len(dataset[split_name]["target"]))
+                )
+
     for split_name, current_split in tqdm(dataset.items(), desc="Split"):
         for source_or_target, data in current_split.items():
-            with open(os.path.join(save_path, split_name+"."+source_or_target), 'a') as f:
-                for item in tqdm(data, desc="Writing " + source_or_target + " Documents"):
+            with open(
+                os.path.join(save_path, split_name + "." + source_or_target), "a"
+            ) as f:
+                for item in tqdm(
+                    data, desc="Writing " + source_or_target + " Documents"
+                ):
                     f.write("%s\n" % item)
                 gc.collect()
+    del dataset
+    gc.collect()
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
@@ -60,7 +92,8 @@ if __name__ == "__main__":
 
     # The path where the articles are to be saved
     path = "arxiv-pubmed"
-    if not os.path.exists(path): os.makedirs(path)
+    if not os.path.exists(path):
+        os.makedirs(path)
 
     write_to_bin(arxiv_articles_dir, path)
     write_to_bin(pubmed_articles_dir, path)
